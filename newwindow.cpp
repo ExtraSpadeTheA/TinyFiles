e81c4cea56090449e202f2ab107dc832b8006f7d
ExtraSpadeTheA <satalice_puno@outlook.com>
2022-06-01 01:12:18 +0800

    new

--- newwindow.cpp
+++ newwindow.cpp
@@ -2,14 +2,14 @@
 #include "ui_newwindow.h"
 #include "fileinfo.h"
 #include <QFileDialog>
+#include <QFileInfo>
+#include <QDateTime>
 
 NewWindow::NewWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::NewWindow)
 {
     ui->setupUi(this);
-    FileInfo info(new QString("name"),new  QString("123kb"),new  QString("txt"),new  QString("2022-6-1"));
-    info.AddRow(ui->files);
     connect(ui->actionUpload_Files,&QAction::triggered,this,&NewWindow::OpenFile);
 }
 void NewWindow::OpenFile(){
@@ -17,6 +17,13 @@ void NewWindow::OpenFile(){
     dialog.setFileMode(QFileDialog::AnyFile);
     dialog.setViewMode(QFileDialog::Detail);
     dialog.exec();
+    QFileInfo info(dialog.selectedFiles()[0]);
+    QString name=info.fileName();
+    QString size=QString::number(info.size());
+    QString type=info.suffix();
+    QString date=info.lastModified().toString();
+    FileInfo tableinfo(&name,&size,&type,&date);
+    tableinfo.AddRow(ui->files);
 
 }
 
